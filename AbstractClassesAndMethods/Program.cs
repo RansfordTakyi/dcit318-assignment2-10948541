f1df0d13b01bf0295c38a9a7ad4f68222cc9ba3f
RansfordTakyi <roduro_takyi@st.ug.edu.gh>
2024-06-15 14:02:06 +0000

    Add Abstract Classes and Methods application

---
+++ AbstractClassesAndMethods/Program.cs
@@ -0,0 +1,52 @@
+﻿
+// AbstractClassesAndMethods/Program.cs
+using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+}
+
+class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double GetArea()
+    {
+        return length * width;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape myCircle = new Circle(5.0);
+        Shape myRectangle = new Rectangle(4.0, 6.0);
+
+        Console.WriteLine($"Circle Area: {myCircle.GetArea()}");
+        Console.WriteLine($"Rectangle Area: {myRectangle.GetArea()}");
+    }
+}
