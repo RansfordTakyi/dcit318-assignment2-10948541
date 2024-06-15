3af082a37c90eb30a7c100046973458f36e52380
RansfordTakyi <roduro_takyi@st.ug.edu.gh>
2024-06-15 14:00:59 +0000

    Add Inheritance and Method Overriding application

---
+++ InheritanceAndOverriding/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// InheritanceAndOverriding/Program.cs
+using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.MakeSound();
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
